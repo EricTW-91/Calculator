b014933b7bc71961fa08900d0dd32c00d0b61bc7
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-03-11 02:08:06 -0800

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+function btInput(value) {
+    document.getElementById('result').innerHTML += value;
+    // document.getElementById('result').value += value;
+}
+
+// $('button').click(()=>{
+//     // $(document).ready(()=>{
+//         // $('#algorithm').append(`${$(this).val()}`)
+//         $('.screen').append(`<h1>hihihi</h1>`)
+//     // })
+// })
