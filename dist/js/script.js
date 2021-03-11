c2b16ea534a4dca4bfc8fa123827fcb1b98579d7
Eric Tseng <amperic05@cengguanyudeMacBook-Pro.local>
2021-03-11 02:49:18 -0800

    caculate function updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,59 @@
+let flag = false;
+// const docGet_result = document.getElementById('result');
+    
+
 function btInput(value) {
-    document.getElementById('result').innerHTML += value;
+    const docGet_result = document.getElementById('result');
+
+    docGet_result.innerHTML += value;
     // document.getElementById('result').value += value;
 }
+
+function clrScreen(){
+    flag = false;
+    const docGet_result = document.getElementById('result');
+
+    docGet_result.innerHTML = '';
+
+    console.log(flag);
+}
+
+// function addition(){
+
+// }
+
+// function subtraction(){
+
+// }
+
+// function multiplication(){
+
+// }
+
+// function division(){
+
+// }
+
+function calculate(value){
+    const docGet_result = document.getElementById('result');
+
+
+
+    if(flag){
+        let result  = eval(docGet_result.innerText);
+        docGet_result.innerHTML = result;
+        flag = false;
+        return;
+    }else{
+        docGet_result.innerHTML += value;
+    }
+
+    flag = true;
+
+    console.log(flag);
+}
+
+
 
 // $('button').click(()=>{
 //     // $(document).ready(()=>{
